1a969245c9eccd9a4de1735a787a9cd19a62f1fd
Diiegohdez <diiego.hdez@gmail.com>
2023-09-27 21:35:09 -0500

    api modificada

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4000/api',
+    baseURL: 'https://back-end-login.onrender.com/api',
     withCredentials: true
 });
 
+//http://localhost:4000/api
+
 export default instance;
+
